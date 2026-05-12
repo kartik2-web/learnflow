789e590b23629c78dc46b1dfa261c60cd8b7a5f0
kartik2-web <01kartikrana@gmail.com>
2026-05-12 17:00:44 +0530

    Fixed production API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
 
-// AUTO ADD TOKEN
-API.interceptors.request.use(
 
+// AUTO ADD TOKEN
+
+API.interceptors.request.use(
   (config) => {
 
     const token =
       localStorage.getItem("token");
 
     if (token) {
-
       config.headers.Authorization =
         `Bearer ${token}`;
     }
@@ -23,7 +25,6 @@ API.interceptors.request.use(
   },
 
   (error) => {
-
     return Promise.reject(error);
   }
 );
